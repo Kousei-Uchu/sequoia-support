48b864297dadee1d781f99b2587a80c5422827d5
Kousei-Uchu <kouseicomms@gmail.com>
2025-04-08 09:57:05 +1000

    Update SensitivityCard.js

--- components/SensitivityCard.js
+++ components/SensitivityCard.js
@@ -1,8 +1,14 @@
 export default function SensitivityCard({ icon, title, description }) {
+  const isFontAwesome = !icon.startsWith('http');
+  
   return (
     <div className="sensitivity-card">
       <div className="sensitivity-header">
-        {icon && <img src={icon} alt="" className="sensitivity-icon" />}
+        {isFontAwesome ? (
+          <i className={`fas fa-${icon} sensitivity-icon`} />
+        ) : (
+          <img src={icon} alt="" className="sensitivity-icon" />
+        )}
         <h3 className="sensitivity-title">{title}</h3>
       </div>
       {description && (
