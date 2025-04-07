b00157d9c514d0718f3fd73791e961d03bb1b362
Kousei-Uchu <kouseicomms@gmail.com>
2025-04-07 17:14:07 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
-// pages/_app.js
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+}) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider 
+      session={session}
+      refetchInterval={5 * 60} // Refresh session every 5 minutes
+      refetchOnWindowFocus={true}
+    >
       <Component {...pageProps} />
     </SessionProvider>
   )
