945aab05a315c603f6ebe4d8dc7526d578d8e90f
Kousei-Uchu <kouseicomms@gmail.com>
2025-04-08 09:42:41 +1000

    Update SensitivityCard.js

--- components/SensitivityCard.js
+++ components/SensitivityCard.js
@@ -3,7 +3,7 @@ export default function SensitivityCard({ icon, title, description }) {
     <div className="sensitivity-card">
       <div className="sensitivity-header">
         {icon && <img src={icon} alt="" className="sensitivity-icon" />}
-        <h3>{title}</h3>
+        <h3 className="sensitivity-title">{title}</h3>
       </div>
       {description && (
         <div className="sensitivity-description">
