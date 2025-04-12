f81ec28d62374a4d32d42d1738723e285bcf92fe
Kousei-Uchu <kouseicomms@gmail.com>
2025-04-12 15:59:23 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['raw.githubusercontent.com'],
+    domains: ['github.com'],
   },
   async rewrites() {
     return [
